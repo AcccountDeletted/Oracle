e8fe77e1bf2d829d865d51d28189836a6aac0fa3
Artem Pavlenko <34988101+ArtemPavlenko@users.noreply.github.com>
2018-01-05 23:01:15 +0100

    Add files via upload

---
+++ Práctica - Nº 2.sql
@@ -0,0 +1,21 @@
+-- Práctica - Nº 2
+
+
+-- 1.
+
+SELECT P.nombre, P.apellido1, P.apellido2
+FROM profesores P JOIN departamentos D ON P.departamento = D.codigo
+WHERE D.codigo = 1;
+
+
+-- 2.
+
+SELECT A.codigo, A.nombre, NVL(TO_CHAR(A.practicos), 'No tiene') "CREDITOS PRACTICOS"
+FROM asignaturas A, alumnos AL, matricular M
+WHERE AL.nombre = 'Nicolas' AND AL.apellido1 = 'Bersabe' AND AL.apellido2 = 'Alba' AND M.alumno = AL.dni AND M.asignatura = A.codigo;
+
+
+-- 3.
+
+
+
